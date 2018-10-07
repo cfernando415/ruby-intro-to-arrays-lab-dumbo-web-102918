fa6a8e0e2b243da8a8d9c52a93fb58c18e00bc57
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-07 03:17:32 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,19 +7,19 @@ def array_with_two_elements
 end
 
 def first_element(arr)
-  
+  arr.first
 end
 
 def third_element(arr)
-  
+  arr[2]  
 end
 
 def last_element(arr)
-  
+  arr.last  
 end
 
 def first_element_with_array_methods
-
+  
 end
 
 def last_element_with_array_methods
